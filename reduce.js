c73a0b5373412387813011764d597909e0637da0
Steve Witman <stevewitman@gmail.com>
2016-03-28 03:25:29 -0600

    classic example

---
+++ reduce.js
@@ -0,0 +1,11 @@
+var data = [15, 3, 20];
+
+var reducer = function(accumulator, item) {
+  return accumulator + item;
+}
+
+var initialValue = 0;
+
+var total = data.reduce(reducer, initialValue);
+
+console.log("Sum:", total);
